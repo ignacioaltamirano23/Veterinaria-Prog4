bca9fafa6d9ca16705616d83ed05181fce330d36
ignacio <ignacioaltamiranom23@gmail.com>
2025-11-01 17:45:38 -0300

    Agrego vista de acceso denegado

---
+++ Veterinaria/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VetTest.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: /Account/AccessDenied
+        public IActionResult AccessDenied(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
